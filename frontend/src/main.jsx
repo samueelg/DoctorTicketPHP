ebe7b622a605491e2721c630760de691b1bbeea8
Samuel Goncalves <samuel.goncalves@edu.unifil.br>
2026-02-09 22:57:08 -0300

    :sparkles: Criando tela de Inicio

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Login from "./pages/Login";
 import "./global.css"
+import Inicio from "./pages/Inicio";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Login />
+    <Inicio />
   </React.StrictMode>,
 );
