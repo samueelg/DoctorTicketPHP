04c82619c5c07fcc5bbdd9d8a2e1ecf14b501565
Samuel <samuelmello1234@hotmail.com>
2026-02-27 11:56:23 -0300

    :sparkles: Adicionando Laravel Breeze ao projeto

--- backend/doctorticket/routes/web.php
+++ backend/doctorticket/routes/web.php
@@ -1,14 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UsuarioController;
-use App\Http\Controllers\HomeController;
 
+Route::get('/', function () {
+    return ['Laravel' => app()->version()];
+});
 
-Route::get('/', [HomeController::class, 'home'])->name('home');
-Route::get('/list', [UsuarioController::class, 'getUsuariosView'])->name('usersList');
-Route::get('/create', [UsuarioController::class, 'getCreateUserView'])->name('createUser');
-Route::post('/createUser', [UsuarioController::class, 'saveUsuario'])->name('createUserSubmit');
-Route::get('/edit/{user}', [UsuarioController::class, 'getEditView'])->name('editUser');
-Route::post('/edit/{user}', [UsuarioController::class, 'editUsuario'])->name('editUserSubmit');
-Route::delete('/delete/{user}', [UsuarioController::class, 'removeUsuario'])->name('deleteUserSubmit');
+require __DIR__.'/auth.php';
